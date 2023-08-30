00ba5a64cb78c669b98e42951e8ab14c3bb8d37a
Ketan Maheshwari <ketancmaheshwari@gmail.com>
2023-08-30 13:36:26 -0400

    first commit

---
+++ randhn.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+platform=$(uname -a)
+
+start_dt=1171864861
+end_dt=$(date +%s)
+
+n=1
+if [[ $# == 1 ]]; then
+	n="$1"
+	if [[ n -gt 5 ]]; then
+		n=5
+	fi
+fi
+
+for each in $(seq 1 "$n"); do
+
+  if [[ $platform =~ "MAC" ]]; then
+    
+    rand_dt=$(jot -r 1 "$start_dt" "$end_dt")
+    dt=$(date -r "$rand_dt" '+%Y-%m-%d')
+    open "https://news.ycombinator.com/front?day=${dt}"
+
+  elif [[ "$platform" =~ "Linux" ]]; then
+    
+    rand_dt=$(shuf -i "$start_dt"-"$end_dt" -n 1)
+    dt=$(date -d "1970-01-01 UTC + \"$rand_dt\" seconds" '+%Y-%m-%d')
+    firefox "https://news.ycombinator.com/front?day=${dt}" &>/dev/null &
+  
+  else
+	  echo "I don't know the platform, quitting."
+	  exit 1
+  fi
+
+  sleep 2
+done
+
